acc72a7860b1be61a3e65201a73e1d00c24bc7be
anasretdinov <anasr25786@yandex.ru>
2023-12-16 12:25:22 +0300

    generator frame

---
+++ Player.py
@@ -0,0 +1,16 @@
+from TextEncoder import TextEncoder
+from Generator import PictureGenerator
+
+class Player:
+    # requirements:
+    #
+    def __init__(self, ):
+        self.encoder = TextEncoder()
+        self.generator = PictureGenerator()
+
+    def draw_pictures(self, prompt: list[str] | str):
+        if type(prompt) != list:
+            prompt = [prompt]
+        text_embeddings = self.encoder.encode(prompt)
+        pictures = self.generator.generate_pictures(text_embeddings)
+        return pictures
