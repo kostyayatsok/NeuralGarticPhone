c3892cead14bb449dcfbeb50c2fdc96156ad3f34
anasretdinov <anasr25786@yandex.ru>
2023-12-17 15:59:49 +0300

    picture describing

--- Player.py
+++ Player.py
@@ -1,12 +1,13 @@
 from TextEncoder import TextEncoder
 from Generator import PictureGenerator
-
-
+from PictureDescriber import PictureDescriber
+from PIL import Image
 class Player:
     def __init__(self, batch_size=1):
         self.batch_size = batch_size
         self.encoder = TextEncoder(batch_size=self.batch_size)
         self.generator = PictureGenerator(batch_size=self.batch_size)
+        self.describer = PictureDescriber()
 
     def draw_pictures(self, prompt):
         if type(prompt) != list:
@@ -14,6 +15,9 @@ class Player:
         text_embeddings = self.encoder.encode(prompt)
         pictures = self.generator.generate_pictures(text_embeddings)
         return pictures
+
+    def describe(self, images):
+        return self.describer.describe(images)
 
 
 def test_drawing(client, prompt):
