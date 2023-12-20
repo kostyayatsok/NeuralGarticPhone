a8386d8d932259b6ea3418998a8f5b39201a955c
anasretdinov <anasr25786@yandex.ru>
2023-12-20 09:44:40 +0300

    немножко переписал для оптимизации

--- Generator.py
+++ Generator.py
@@ -14,12 +14,16 @@ sdpath = "stabilityai/stable-diffusion-2"
 
 
 class PictureGenerator:
+    inited = False
+    pipe = None
     def __init__(self, device="cuda" if torch.cuda.is_available() else "cpu", steps=25, guidance_scale=7.5, path="sd-concepts-library/gphone01"):
+        if not PictureGenerator.inited:
+          PictureGenerator.pipe = StableDiffusionPipeline.from_pretrained(sdpath, torch_dtype=torch.float16).to(device)
+          PictureGenerator.pipe.load_textual_inversion(path)
+          inited = True
         self.device = device
         self.steps = steps
         self.scale = guidance_scale
-        self.pipe = StableDiffusionPipeline.from_pretrained(sdpath, torch_dtype=torch.float16).to(device)
-        self.pipe.load_textual_inversion(path)
 
     def generate_pictures(self, prompt):
         images = self.pipe(prompt, num_images_per_prompt=1, num_inference_steps=self.steps, guidance_scale=self.scale).images
