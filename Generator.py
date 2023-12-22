4177e098822f0625de8c6e4a44dac30beae65e60
anasretdinov <anasr25786@yandex.ru>
2023-12-22 22:09:16 +0300

    - обновил версию модельки - убрал чб (теперь без него все хорошо) - подтюнил промпт и количество шагов

--- Generator.py
+++ Generator.py
@@ -16,7 +16,7 @@ sdpath = "stabilityai/stable-diffusion-2"
 class PictureGenerator:
     inited = False
     pipe = None
-    def __init__(self, device="cuda" if torch.cuda.is_available() else "cpu", steps=25, guidance_scale=7.5, path="sd-concepts-library/gphone01"):
+    def __init__(self, device="cuda" if torch.cuda.is_available() else "cpu", steps=25, guidance_scale=7.5, path="sd-concepts-library/gphone03"):
         if not PictureGenerator.inited:
           PictureGenerator.pipe = StableDiffusionPipeline.from_pretrained(sdpath, torch_dtype=torch.float16).to(device)
           PictureGenerator.pipe.load_textual_inversion(path)
@@ -40,4 +40,4 @@ class PictureGenerator:
         return _
     def generate_pictures(self, prompt):
         images = self.pipe(prompt, num_images_per_prompt=1, num_inference_steps=self.steps, guidance_scale=self.scale).images
-        return self.make_bw(images)
+        return images
