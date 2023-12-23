51508d79235cff7ba0c534e0bc0df66aa7173010
anasretdinov <anasr25786@yandex.ru>
2023-12-23 10:13:05 +0300

    fixed bug

--- PictureDescriber.py
+++ PictureDescriber.py
@@ -8,12 +8,13 @@ class PictureDescriber:
     model = None
     feature_extractor = None
     tokenizer = None
+    device = None
     def __init__(self):
         if not PictureDescriber.inited:
           inited = True
-          device="cuda" if torch.cuda.is_available() else "cpu"
+          PictureDescriber.device = "cuda" if torch.cuda.is_available() else "cpu"
           PictureDescriber.model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-          PictureDescriber.model = PictureDescriber.model.to(device)
+          PictureDescriber.model = PictureDescriber.model.to(PictureDescriber.device)
           PictureDescriber.tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
           PictureDescriber.feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
         self.max_length = 16
@@ -24,7 +25,7 @@ class PictureDescriber:
         images = [i.convert(mode="RGB") for i in i_images]
 
         pixel_values = self.feature_extractor(images=images, return_tensors="pt").pixel_values
-        pixel_values = pixel_values.to(self.device)
+        pixel_values = pixel_values.to(PictureDescriber.device)
         output_ids = self.model.generate(pixel_values, **self.gen_kwargs)
         preds = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
         preds = [pred.strip() for pred in preds]
