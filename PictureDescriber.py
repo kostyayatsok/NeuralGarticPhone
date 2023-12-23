583118ae229d0b9676f540649853896cfc5b35d7
anasretdinov <anasr25786@yandex.ru>
2023-12-23 11:00:20 +0300

    changed image captioning to git-base

--- PictureDescriber.py
+++ PictureDescriber.py
@@ -1,40 +1,39 @@
-from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+from transformers import AutoProcessor, AutoModelForCausalLM
+
+import requests
 import torch
 from PIL import Image
-
 
 class PictureDescriber:
     inited = False
     model = None
     feature_extractor = None
     tokenizer = None
-    device = None
     def __init__(self):
         if not PictureDescriber.inited:
           inited = True
-          PictureDescriber.device = "cuda" if torch.cuda.is_available() else "cpu"
-          PictureDescriber.model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+          PictureDescriber.device="cuda" if torch.cuda.is_available() else "cpu"
+          PictureDescriber.processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
+          PictureDescriber.model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
           PictureDescriber.model = PictureDescriber.model.to(PictureDescriber.device)
-          PictureDescriber.tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-          PictureDescriber.feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        self.max_length = 16
-        self.num_beams = 4
-        self.gen_kwargs = {"max_length": self.max_length, "num_beams": self.num_beams}
 
+    def __describe_one(self, image):
+        pixel_values = PictureDescriber.processor(images=image, return_tensors="pt").pixel_values
+        pixel_values = pixel_values.to(PictureDescriber.device)
+        generated_ids = PictureDescriber.model.generate(pixel_values=pixel_values, max_length=50)
+        generated_caption = PictureDescriber.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return generated_caption
     def describe(self, i_images):
         images = [i.convert(mode="RGB") for i in i_images]
-
-        pixel_values = self.feature_extractor(images=images, return_tensors="pt").pixel_values
-        pixel_values = pixel_values.to(PictureDescriber.device)
-        output_ids = self.model.generate(pixel_values, **self.gen_kwargs)
-        preds = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-        preds = [pred.strip() for pred in preds]
-        return preds
+        res = []
+        for i in images:
+            res.append(self.__describe_one(i))
+        return res
 
 
 # desc = PictureDescriber()
 # with Image.open("cat.jpg") as img:
-#     res = desc.describe([img])
+#     res = desc.describe(img)
 #     print(res)
 # ['a gray and white cat is looking at the camera']
 
